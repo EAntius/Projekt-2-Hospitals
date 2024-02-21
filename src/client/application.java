688d584fb3bec912fa09c4d748c44d1c232b345f
Hugo Orrberg <hugo@debian-BULLSEYE-live-builder-AMD64>
2024-02-21 13:21:03 +0100

    started working on write and create commands

--- src/client/application.java
+++ src/client/application.java
@@ -1,6 +1,9 @@
 package client;
 
+import java.io.IOException;
+
 public class application {
+    public static swingTextEditor editor = new swingTextEditor();
     public static void main(String[] args) throws Exception {
         while (true) {
             String[] loginCredentials = login.getUserCredentials();
@@ -22,10 +25,43 @@ public class application {
                   break;
                 }
                 conn.send(msg);
-                System.out.println(conn.getResponse());
+
+                switch (msg.split(" ")[0]) {
+                    case "write":
+                        write(conn, msg);
+                        break;
+                    case "create":
+                        create(conn, msg);
+                        break;
+                    default:
+                        System.out.println(conn.getResponse());
+                        break;
+                }
             }
             break;
         }
+    }
+
+    public static void write(connection conn, String msg) throws IOException, InterruptedException {
+        String text = conn.getResponse();
+        if (text == "You don't have privileges for this action") {
+            System.out.println(text);
+            return;
+        }
+        String editedText = editor.openTextEditor(text);
+        conn.send(editedText);
+    }
+
+    public static void create(connection conn, String msg) throws IOException, InterruptedException {
+        String text = conn.getResponse();
+        if (text == "You don't have privileges for this action") {
+            System.out.println(text);
+            return;
+        }
+
+        //TODO - somehow parse response and create file
+        String editedText = editor.openTextEditor(text);
         
+        conn.send(editedText);
     }
 }

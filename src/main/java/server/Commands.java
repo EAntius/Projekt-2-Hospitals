13971482da7b44b67959ea933088241711b5487d
Edvin Antius <ed6866an-s@student.lu.se>
2024-02-21 09:23:32 +0100

    new commmands class added

---
+++ src/main/java/server/Commands.java
@@ -0,0 +1,33 @@
+package java.server;
+
+public class Commands {
+
+    public Commands() {
+    }
+    
+    public void execute(String[] command){
+        switch(command[0]){
+            case "read":
+
+            break;
+            case "write":
+
+            break;
+
+            case "delete":
+
+            break;
+            case "create":
+
+            break;
+
+            case "available":
+
+            break;
+            default:
+
+            break;
+        }
+    }
+}
+
---
+++ src/main/java/server/server.java
@@ -0,0 +1,123 @@
+package java.server;
+
+import java.io.*;
+import java.net.*;
+import javax.net.*;
+import javax.net.ssl.*;
+import java.security.KeyStore;
+import java.security.cert.Certificate;
+import java.security.cert.X509Certificate;
+import java.util.Scanner;
+
+public class server implements Runnable {
+  private ServerSocket serverSocket = null;
+  private static int numConnectedClients = 0; 
+  
+  public server(ServerSocket ss) throws IOException {
+    serverSocket = ss;
+    newListener();
+  }
+
+  public void run() {
+    try {
+      SSLSocket socket=(SSLSocket)serverSocket.accept();
+      newListener();
+      SSLSession session = socket.getSession();
+      Certificate[] cert = session.getPeerCertificates();
+      String subject = ((X509Certificate) cert[0]).getSubjectX500Principal().getName();
+      String issuer = ((X509Certificate) cert[0]).getIssuerDN().getName();
+      String serial = ((X509Certificate) cert[0]).getSerialNumber().toString();
+      numConnectedClients++;
+      System.out.println("client connected");
+      System.out.println("client name (cert subject DN field): " + subject);
+      System.out.println("client issuer: " + issuer);
+      System.out.println("client serialnr: " + serial);
+      System.out.println(numConnectedClients + " concurrent connection(s)\n");
+      String[] userdata = findUser(subject);
+      String subjectRole = userdata[1];
+      String subjectAttribute = userdata[2];
+      
+      PrintWriter out = null;
+      BufferedReader in = null;
+      out = new PrintWriter(socket.getOutputStream(), true);
+      in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+
+      String clientMsg = null;
+      while ((clientMsg = in.readLine()) != null) {
+        String[] recieved = clientMsg.split(" ");
+        
+        
+        System.out.println("done\n");
+      }
+      in.close();
+      out.close();
+      socket.close();
+      numConnectedClients--;
+      System.out.println("client disconnected");
+      System.out.println(numConnectedClients + " concurrent connection(s)\n");
+    } catch (IOException e) {
+      System.out.println("Client died: " + e.getMessage());
+      e.printStackTrace();
+      return;
+    }
+  }
+  
+  private void newListener() { (new Thread(this)).start(); } // calls run()
+  public static void main(String args[]) {
+    System.out.println("\nServer Started\n");
+    int port = -1;
+    if (args.length >= 1) {
+      port = Integer.parseInt(args[0]);
+    }
+    String type = "TLSv1.2";
+    try {
+      ServerSocketFactory ssf = getServerSocketFactory(type);
+      ServerSocket ss = ssf.createServerSocket(port);
+      ((SSLServerSocket)ss).setNeedClientAuth(true); // enables client authentication
+      new server(ss);
+    } catch (IOException e) {
+      System.out.println("Unable to start Server: " + e.getMessage());
+      e.printStackTrace();
+    }
+  }
+
+  private static ServerSocketFactory getServerSocketFactory(String type) {
+    if (type.equals("TLSv1.2")) {
+      SSLServerSocketFactory ssf = null;
+      try { // set up key manager to perform server authentication
+        SSLContext ctx = SSLContext.getInstance("TLSv1.2");
+        KeyManagerFactory kmf = KeyManagerFactory.getInstance("SunX509");
+        TrustManagerFactory tmf = TrustManagerFactory.getInstance("SunX509");
+        KeyStore ks = KeyStore.getInstance("JKS");
+        KeyStore ts = KeyStore.getInstance("JKS");
+        char[] password = "password".toCharArray();
+        // keystore password (storepass)
+        ks.load(new FileInputStream("serverkeystore"), password);  
+        // truststore password (storepass)
+        ts.load(new FileInputStream("servertruststore"), password); 
+        kmf.init(ks, password); // certificate password (keypass)
+        tmf.init(ts);  // possible to use keystore as truststore here
+        ctx.init(kmf.getKeyManagers(), tmf.getTrustManagers(), null);
+        ssf = ctx.getServerSocketFactory();
+        return ssf;
+      } catch (Exception e) {
+        e.printStackTrace();
+      }
+    } else {
+      return ServerSocketFactory.getDefault();
+    }
+    return null;
+  }
+
+  private String[] findUser(String subject) {
+    Scanner scan = new Scanner("./hospitaldatabase/userdatabase/users.txt");
+    while(scan.hasNext()) {
+        String line = scan.nextLine().toString();
+        String[] data = line.split(" ");
+        if (data[0].compareTo(subject) == 0) {
+            return data;
+        }
+    }
+    return null;
+  }
+}
